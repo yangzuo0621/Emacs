a3f8bba9ef7c6b1c95f224529c8b15023f069362
Zuo Yang <zuya@microsoft.com>
2021-05-20 15:52:47 +0800

    install try and which-key package

--- init.el
+++ init.el
@@ -11,6 +11,13 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(use-package try
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
 (global-display-line-numbers-mode)
 
 (add-hook 'after-init-hook 'global-company-mode)
@@ -21,7 +28,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package company)))
+ '(package-selected-packages '(which-key try use-package company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
