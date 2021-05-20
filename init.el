5fb2e432a7eb8a926c23865ea31b76ad592e6e85
Zuo Yang <zuya@microsoft.com>
2021-05-20 15:50:17 +0800

    add  use-package

--- init.el
+++ init.el
@@ -7,8 +7,24 @@
 
 (package-initialize)
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 (global-display-line-numbers-mode)
 
 (add-hook 'after-init-hook 'global-company-mode)
 
 (setq make-backup-files nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
