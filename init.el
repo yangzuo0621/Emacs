618d3e6fd98ad38a2fab6992ee6e7b74f437e5ed
Zuo Yang <zuya@microsoft.com>
2021-05-20 20:06:57 +0800

    disable welcome emacs page

--- init.el
+++ init.el
@@ -1,4 +1,19 @@
-;; (setq inhibit-startup-message t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(which-key try use-package company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Our stuff
+
+(setq inhibit-startup-message t)
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -29,15 +44,3 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
 (setq make-backup-files nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key try use-package company)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
