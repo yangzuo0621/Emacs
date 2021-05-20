86b612ec57064615438895e681e06e5229078d60
Zuo Yang <zuya@microsoft.com>
2021-05-20 15:48:28 +0800

    add melpa package archive

--- init.el
+++ init.el
@@ -1,3 +1,12 @@
+;; (setq inhibit-startup-message t)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
 (global-display-line-numbers-mode)
 
 (add-hook 'after-init-hook 'global-company-mode)
