745b777bd607dc3cf5ad558c315d57d22a6975e4
Zuo Yang <zuya@microsoft.com>
2021-07-24 23:12:05 +0800

    iedit and narrow /widen dwim

--- myinit.el
+++ myinit.el
@@ -149,24 +149,60 @@
 ; (windmove-default-keybindings)
 
 ; highlight the select line
-  (global-hl-line-mode t)
+(global-hl-line-mode t)
 
-  (use-package beacon
-    :ensure t
-    :config
-    (beacon-mode 1))
+(use-package beacon
+  :ensure t
+  :config
+  (beacon-mode 1))
 
-  (use-package hungry-delete
-    :ensure t
-    :config
-    (global-hungry-delete-mode))
+(use-package hungry-delete
+  :ensure t
+  :config
+  (global-hungry-delete-mode))
 
-  ;  (use-package aggressive-indent
-  ;    :ensure t
-  ;    :config
-  ;    (global-aggressive-indent-mode 1))
+;(use-package aggressive-indent
+;  :ensure t
+;  :config
+;  (global-aggressive-indent-mode 1))
 
-  (use-package expand-region
-    :ensure t
-    :config
-    (global-set-key (kbd "C-]") 'er/expand-region))
+(use-package expand-region
+  :ensure t
+  :config
+  (global-set-key (kbd "C-]") 'er/expand-region))
+
+(use-package iedit
+  :ensure t
+  :config
+  (global-set-key (kbd "C-c ;") 'iedit-mode))
+
+(defun narrow-or-widen-dwim (p)
+  "Widen if buffer is narrowed, narrow-dwim otherwise.
+Dwim means: region, org-src-block, org-subtree, or defun,
+whichever applies first. Narrowing to org-src-block actually
+calls `org-edit-src-code'.
+
+With prefix P, don't widen, just narrow even if buffer is already
+narrowed."
+
+(interactive "P")
+(declare (interactive-only))
+(cond ((and (buffer-narrowed-p) (not p)) (widen))
+      ((region-active-p)
+       (narrow-to-region (region-beginning) (region-end)))
+      ((derived-mode-p 'org-mode)
+       ;; `org-edit-src-code' is not a real narrowing
+       ;; command. Remove this first conditional if
+       ;; you don't want it.
+       (cond ((ignore-errors (org-edit-src-code) t)
+	      (delete-other-windows))
+	     ((ignore-errors (org-narrow-to-block) t))
+	     (t (org-narrow-to-subtree))))
+      ((derived-mode-p 'latex-mode)
+       (LaTeX-narrow-to-environment))
+      (t (narrow-to-defun))))
+
+;; (define-key endless/toggle-map "n" #'narrow-or-widen-dwim)
+;; This line actually replaces Emacs' entire narrowing keymap, that's
+;; how much I like this command. Only copy it if that's what you want.
+(define-key ctl-x-map "n" #'narrow-or-widen-dwim)
