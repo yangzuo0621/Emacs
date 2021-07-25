e8923b5761b6165030aa2397e7da6926defaea05
Zuo Yang <zuya@microsoft.com>
2021-07-25 20:58:41 +0800

    emacs tree file explorer

--- myinit.el
+++ myinit.el
@@ -180,60 +180,12 @@
 ;  :bind (("C-'" . better-shell-shell)
 ;	    ("C-c [" . better-shell-remote-open)))
 
-(use-package hydra
+(use-package projectile
   :ensure t
-  :init
-  (global-set-key
-   (kbd "C-x t")
-   (defhydra toggle (:color blue)
-     "toggle"
-     ("a" abbrev-mode "abbrev")
-     ("s" flyspell-mode "flyspell")
-     ("d" toggle-debug-on-error "debug")
-     ("c" fci-mode "fCi")
-     ("f" auto-fill-mode "fill")
-     ("t" toggle-truncate-lines "truncate")
-     ("w" whitespace-mode "whitespace")
-     ("q" nil "cancel")))
-  (global-set-key
-     (kbd "C-x j")
-     (defhydra gotoline 
-     (:pre (linum-mode 1)
-     :post (linum-mode -1))
-     "goto"
-     ("t" (lambda () (interactive)(move-to-window-line-top-bottom 0)) "top")
-     ("b" (lambda () (interactive)(move-to-window-line-top-bottom -1)) "bottom")
-     ("m" (lambda () (interactive)(move-to-window-line-top-bottom)) "middle")
-     ("e" (lambda () (interactive)(end-of-buffer)) "end")
-     ("c" recenter-top-bottom "recenter")
-     ("n" next-line "down")
-     ("p" (lambda () (interactive) (forward-line -1))  "up")
-     ("g" goto-line "goto-line")
-     ))
-  (global-set-key
-   (kbd "C-c t")
-   (defhydra hydra-global-org (:color blue)
-     "Org"
-     ("t" org-timer-start "Start Timer")
-     ("s" org-timer-stop "Stop Timer")
-     ("r" org-timer-set-timer "Set Timer") ; This one requires you be in an orgmode doc, as it sets the timer for the header
-     ("p" org-timer "Print Timer") ; output timer value to buffer
-     ("w" (org-clock-in '(4)) "Clock-In") ; used with (org-clock-persistence-insinuate) (setq org-clock-persist t)
-     ("o" org-clock-out "Clock-Out") ; you might also want (setq org-log-note-clock-out t)
-     ("j" org-clock-goto "Clock Goto") ; global visit the clocked task
-     ("c" org-capture "Capture") ; Don't forget to define the captures you want http://orgmode.org/manual/Capture.html
-     ("l" (or )rg-capture-goto-last-stored "Last Capture"))
-   ))
-
-(use-package ag
-  :ensure t)
-
-;(use-package projectile
-;  :ensure t
-;  :bind ("C-c p" . projectile-command-map)
-;  :config
-;  (projectile-global-mode)
-;  (setq projectile-completion-system 'ivy))
+  :bind ("C-c p" . projectile-command-map)
+  :config
+  (projectile-global-mode)
+  (setq projectile-completion-system 'ivy))
 
 ;(use-package dumb-jump
 ;  :bind (("M-g o" . dumb-jump-go-other-window)
@@ -245,6 +197,103 @@
 ;  :init
 ;  (dumb-jump-mode)
 ;  :ensure)
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+	  treemacs-deferred-git-apply-delay      0.5
+	  treemacs-directory-name-transformer    #'identity
+	  treemacs-display-in-side-window        t
+	  treemacs-eldoc-display                 t
+	  treemacs-file-event-delay              5000
+	  treemacs-file-extension-regex          treemacs-last-period-regex-value
+	  treemacs-file-follow-delay             0.2
+	  treemacs-file-name-transformer         #'identity
+	  treemacs-follow-after-init             t
+	  treemacs-expand-after-init             t
+	  treemacs-git-command-pipe              ""
+	  treemacs-goto-tag-strategy             'refetch-index
+	  treemacs-indentation                   2
+	  treemacs-indentation-string            " "
+	  treemacs-is-never-other-window         nil
+	  treemacs-max-git-entries               5000
+	  treemacs-missing-project-action        'ask
+	  treemacs-move-forward-on-expand        nil
+	  treemacs-no-png-images                 nil
+	  treemacs-no-delete-other-windows       t
+	  treemacs-project-follow-cleanup        nil
+	  treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+	  treemacs-position                      'left
+	  treemacs-read-string-input             'from-child-frame
+	  treemacs-recenter-distance             0.1
+	  treemacs-recenter-after-file-follow    nil
+	  treemacs-recenter-after-tag-follow     nil
+	  treemacs-recenter-after-project-jump   'always
+	  treemacs-recenter-after-project-expand 'on-distance
+	  treemacs-litter-directories            '("/node_modules" "/.venv" "/.cask")
+	  treemacs-show-cursor                   nil
+	  treemacs-show-hidden-files             t
+	  treemacs-silent-filewatch              nil
+	  treemacs-silent-refresh                nil
+	  treemacs-sorting                       'alphabetic-asc
+	  treemacs-space-between-root-nodes      t
+	  treemacs-tag-follow-cleanup            t
+	  treemacs-tag-follow-delay              1.5
+	  treemacs-user-mode-line-format         nil
+	  treemacs-user-header-line-format       nil
+	  treemacs-width                         35
+	  treemacs-width-is-initially-locked     t
+	  treemacs-workspace-switch-cleanup      nil)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+    (pcase (cons (not (null (executable-find "git")))
+		 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+	("M-0"       . treemacs-select-window)
+	("C-x t 1"   . treemacs-delete-other-windows)
+	("C-x t t"   . treemacs)
+	("C-x t B"   . treemacs-bookmark)
+	("C-x t C-t" . treemacs-find-file)
+	("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :after (treemacs dired)
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
+(use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
+  :after (treemacs persp-mode) ;;or perspective vs. persp-mode
+  :ensure t
+  :config (treemacs-set-scope-type 'Perspectives))
 
 ;(load-theme 'zenburn t)
 (use-package zenburn-theme
