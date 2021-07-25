576fbc5fd08556cc68aefb55ae8036c3b221cf70
Zuo Yang <zuya@microsoft.com>
2021-07-25 08:42:10 +0000

    iBuffer

--- myinit.el
+++ myinit.el
@@ -29,8 +29,42 @@
 (setq ido-everywhere t)
 (ido-mode 1)
 
-(defalias 'list-buffers 'ibuffer)
+; (defalias 'list-buffers 'ibuffer)
 ; (defalias 'list-buffers 'ibuffer-other-window)
+  (global-set-key (kbd "C-x C-b") 'ibuffer) ;; Use Ibuffer for Buffer List
+  (setq ibuffer-saved-filter-groups
+	(quote (("default"
+		 ("dired" (mode . dired-mode))
+		 ("org" (name . "^.*org$"))
+		 ("magit" (mode . magit-mode))
+		 ("IRC" (or (mode . circe-channel-mode) (mode . circe-server-mode)))
+		 ("web" (or (mode . web-mode) (mode . js2-mode)))
+		 ("shell" (or (mode . eshell-mode) (mode . shell-mode)))
+		 ("mu4e" (or
+			  (mode . mu4e-compose-mode)
+			  (name . "\*mu4e\*")
+			  ))
+		 ("programming" (or
+				 (mode . clojure-mode)
+				 (mode . clojurescript-mode)
+				 (mode . python-mode)
+				 (mode . c++-mode)))
+		 ("emacs" (or
+			   (name . "^\\*scratch\\*$")
+			   (name . "^\\*Messages\\*$")))
+		 ))))
+  (add-hook 'ibuffer-mode-hook
+	    (lambda ()
+	      (ibuffer-auto-mode 1)
+	      (ibuffer-switch-to-saved-filter-groups "default")))
+
+  ;; don't show these
+  ;  (add-to-list 'ibuffer-never-show-predicates "zowie")
+  ;; Don't show filter groups if there are no buffers in that group
+  (setq ibuffer-show-empty-filter-groups nil)
+
+  ;; Don't ask for confirmation to delete marked buffers
+  (setq ibuffer-expert t)
 
 (use-package ace-window
   :ensure t
