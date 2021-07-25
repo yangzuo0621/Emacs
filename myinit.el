ef0ee7af8aa083c5015198391dcf777e5bb259bd
Zuo Yang <zuya@microsoft.com>
2021-07-25 22:30:42 +0800

    dired settings

--- myinit.el
+++ myinit.el
@@ -295,6 +295,32 @@
   :ensure t
   :config (treemacs-set-scope-type 'Perspectives))
 
+(setq dired-dwim-target t)
+
+(use-package dired-narrow
+  :ensure t
+  :config
+  (bind-key "C-c C-n" #'dired-narrow)
+  (bind-key "C-c C-f" #'dired-narrow-fuzzy)
+  (bind-key "C-x C-N" #'dired-narrow-regexp)
+  )
+
+(use-package dired-subtree :ensure t
+  :after dired
+  :config
+  (bind-key "<tab>" #'dired-subtree-toggle dired-mode-map)
+  (bind-key "<backtab>" #'dired-subtree-cycle dired-mode-map))
+
+(use-package pcre2el
+  :ensure t
+  :config
+  (pcre-mode))
+
+(use-package wgrep
+  :ensure t)
+
+; (setq counsel-fzf-cmd "~/.fzf/bin/fzf -f %s")
+
 ;(load-theme 'zenburn t)
 (use-package zenburn-theme
   :ensure t
