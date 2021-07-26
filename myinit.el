027d43879cba007335e170a6139ed8e9aa091609
Zuo Yang <zuya@microsoft.com>
2021-07-26 11:43:48 +0800

    setup LSP mode

--- myinit.el
+++ myinit.el
@@ -135,6 +135,34 @@
   :init
   (global-flycheck-mode))
 
+(use-package lsp-mode
+  :ensure t
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e.g. python-mode)
+	 (python-mode . lsp)
+	 ;; if you want which-key integration
+	 (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+;; if you are helm user
+; (use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; optionally if you want to use debugger
+; (use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+  :config
+  (which-key-mode))
+
 (setq py-python-command "python3")
 (setq python-shell-interpreter "python3")
 (setq flycheck-python-pylint-executable "python3")
@@ -159,11 +187,11 @@
 ;  :init
 ;  (elpy-enable))
 
-(use-package lsp-mode
-  :ensure t
-  :config
-  (add-hook 'python-mode-hook #'lsp)
-  (setq lsp-enable-snippet nil))
+;(use-package lsp-mode
+;  :ensure t
+;  :config
+;  (add-hook 'python-mode-hook #'lsp)
+;  (setq lsp-enable-snippet nil))
 
 (use-package yasnippet
   :ensure t
